72f295ac3ade938f2177cfd6c5ba3f612cd03c20
Alistair Norris <anorris.design@gmail.com>
2024-01-25 17:49:23 +0800

    test

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
-// Catch all other routes and return the index file
+// Send index.html for any other requests
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Set port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
